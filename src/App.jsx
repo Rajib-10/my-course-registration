a43e0eb38e6cae888cfd1d792e2e586ebc29585d
Rajib <rajib@ubuntu>
2023-09-14 19:57:54 +0600

    Environment setup using tailwind and daisy

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1 className="text-3xl text-center m-5 p-5 font-bold">Course Registration</h1>
+      </div>
+       
+    </>
+  )
+}
+
+export default App
