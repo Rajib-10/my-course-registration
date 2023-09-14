a382f529143b588d9d0e31e8e50525791f80b29d
Rajib <rajib@ubuntu>
2023-09-15 00:55:45 +0600

    Functionality of total and remaining credit added

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,16 @@ import Carts from "./Components/Carts/Carts"
 
 function App() {
 const [selectedCourse,setSelectedCourse] = useState([])
+const [countCredit,setCountCredit] = useState(0)
+const [remaining,setRemaining] = useState(0)
+const [countPrice,setCountPrice] = useState(0)
+
 
 const handleSelectButton=(course)=>{
-  const newSelectedCourse = [...selectedCourse,course]
+  
+  let totalCredit=course.credit
+  let totalPrice = course.price
+ 
   const isExist = selectedCourse.find(item=>item.course_name===course.course_name)
   if(isExist){
     return Swal.fire({
@@ -15,8 +22,29 @@ const handleSelectButton=(course)=>{
       title: 'Oops...',
       text: `${course.course_name} Already Exist`
     })
+  }else{
+      selectedCourse.forEach(item=>{
+        totalCredit+=item.credit
+        totalPrice+=item.price
+      })
+      let remainingCredit = 20 - totalCredit
+      if(totalCredit>20 || remainingCredit <0){
+        return Swal.fire({
+          icon: 'error',
+          title: 'Oops...',
+          text: 'Check Credit please!!',
+        })
+      }else{
+        setCountCredit(totalCredit)
+        setRemaining(remainingCredit)
+        setCountPrice(totalPrice)
+      
+      const newSelectedCourse = [...selectedCourse,course]
+      setSelectedCourse(newSelectedCourse)
   }
-  setSelectedCourse(newSelectedCourse)
+      }
+      
+  
 }
   return (
     <>
@@ -24,7 +52,7 @@ const handleSelectButton=(course)=>{
         <h1 className="text-3xl text-center m-5 p-5 font-bold">Course Registration</h1>
         <div className="flex justify-between">
         <Cards handleSelectButton={handleSelectButton} />
-        <Carts selectedCourse={selectedCourse} />
+        <Carts selectedCourse={selectedCourse} countPrice={countPrice} countCredit={countCredit} remaining={remaining}  />
         </div>
       </div>
 
--- src/Components/Carts/Carts.jsx
+++ src/Components/Carts/Carts.jsx
@@ -1,22 +1,22 @@
 /* eslint-disable react/prop-types */
 
 
-const Carts = ({selectedCourse}) => {
-    
+const Carts = ({selectedCourse,countCredit,remaining,countPrice}) => {
     return (
         <div className="w-[25%] mx-4">
         <div className="card card-compact  bg-base-100 shadow-xl">
             <div className="card-body">
-                <h1 className="text-lg font-semibold text-purple-700">Credit Hour Remaining hr</h1>
+                <h1 className="text-lg font-semibold text-purple-700">Credit Hour Remaining {remaining}hr</h1>
                 <hr />
                 <h2 className="card-title">Course Name</h2>
+                <hr />
                 {
                     selectedCourse.map((courseName,idx)=> <h1 className="text-sm font-medium" key={idx}>{idx+1} {courseName.course_name}</h1>) 
                 }
                 <hr />
-                <h1>Total Credit Hour : 13</h1>
+                <h1>Total Credit Hour : {countCredit}</h1>
                 <hr />
-                <h1>Total Price : 48000 USD</h1>
+                <h1>Total Price : {countPrice} USD</h1>
             </div>
         </div>
         </div>
